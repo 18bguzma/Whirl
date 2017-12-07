b6cc1cc3038f5d032cbe132b8384cbfb9abe3de9
18bguzma <guzman.brandon12@Live.com>
2017-12-07 10:44:17 -0600

    V 1.0.1 (Firebase & CocoaPods support)
    + Firebase Functionality
    + CocoaPods Functionality
    + SVProgressHUD

---
+++ Whirl/Whirl/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Whirl
+//
+//  Created by HSOIMAC-01 on 12/7/17.
+//  Copyright © 2017 WhirlDevTeam. All rights reserved.
+//
+
+import UIKit
+import SVProgressHUD
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        SVProgressHUD.setDefaultMaskType(.black)
+        SVProgressHUD.show(withStatus: "Loading Assets...")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
