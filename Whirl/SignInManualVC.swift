6f37ffe4bbb52cdf7aa778e765100901a5a0fbea
18bguzma <guzman.brandon12@Live.com>
2018-01-08 14:56:57 -0600

    Added Navigation Controller

--- Whirl/SignInManualVC.swift
+++ Whirl/SignInManualVC.swift
@@ -22,7 +22,9 @@ class SignInManualVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
